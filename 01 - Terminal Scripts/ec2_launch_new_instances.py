c8f547acbbb5403054279b6f380665fbe2b1fe9f
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-10 12:27:14 -0500

    Create EC2 launch new instances script

---
+++ 01 - Terminal Scripts/ec2_launch_new_instances.py
@@ -0,0 +1,7 @@
+# Launch a specified number of new instances from defined AMI & instance type
+
+# create boto3 ec2 client
+
+# provide AMI image id, instance type, & number to create
+
+# print instance ids to indicate end of script
