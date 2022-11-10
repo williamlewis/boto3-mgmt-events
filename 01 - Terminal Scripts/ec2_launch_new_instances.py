4ea1fd16cbd54fd95f6ab81cc55853013ea596ea
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-10 12:32:22 -0500

    Finalize EC2 launch instances script

--- 01 - Terminal Scripts/ec2_launch_new_instances.py
+++ 01 - Terminal Scripts/ec2_launch_new_instances.py
@@ -1,7 +1,18 @@
 # Launch a specified number of new instances from defined AMI & instance type
 
 # create boto3 ec2 client
+import boto3
+client = boto3.client('ec2')
 
-# provide AMI image id, instance type, & number to create
+# launch instances by providing AMI image id, instance type, & number
+resp = client.run_instances(
+    ImageId='ami-026b57f3c383c2eec',
+    InstanceType='t2.micro',
+    MinCount=1,
+    MaxCount=1
+)
 
 # print instance ids to indicate end of script
+for instance in resp['Instances']:
+    new_inst_id = instance['InstanceId']
+    print(f'New instance {new_inst_id} has been launched.')
