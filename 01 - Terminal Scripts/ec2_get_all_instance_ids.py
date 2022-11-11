558a95207a37da61c2fc507e41c3504ee053ee23
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-11 10:06:41 -0500

    Finalize EC2 get instance ids script

--- 01 - Terminal Scripts/ec2_get_all_instance_ids.py
+++ 01 - Terminal Scripts/ec2_get_all_instance_ids.py
@@ -1,5 +1,16 @@
 # List out InstanceId attribute for all current instances
 
 # create boto3 ec2 resource
+import boto3
+ec2 = boto3.resource('ec2')
+
 # use resource collection to get all instances
-# print ids & indicate end of script
+instances = ec2.instances.all()
+
+# print ids to indicate end of script
+if len(list(instances)) > 0:
+    print('Current EC2 Instances:')
+    for instance in instances:
+        print(instance.instance_id)
+else:
+    print('There are currently no EC2 instances.')
