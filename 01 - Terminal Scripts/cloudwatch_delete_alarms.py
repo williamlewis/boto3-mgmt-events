34ef599d3c0b682da78a94224afb607460a52d10
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-11 19:42:24 -0500

    Finalize CloudWatch delete alarms script

--- 01 - Terminal Scripts/cloudwatch_delete_alarms.py
+++ 01 - Terminal Scripts/cloudwatch_delete_alarms.py
@@ -1,6 +1,18 @@
 # Delete CloudWatch metric alarms
 
 # create boto3 resource for CloudWatch (NOT CloudWatchLogs)
+import boto3
+cloudwatch = boto3.resource('cloudwatch')
+
 # use collection to get all alarms
+alarms = cloudwatch.alarms.all()
+
 # use Alarm class to delete those found
-# print alarm name for those deleted to indicate end of script
+for alarm in alarms:
+    alarm.delete()
+    print(f'{alarm.name} has been deleted.')
+
+# check number of alarms remaining & print number to indicate end of script
+remaining_alarms = cloudwatch.alarms.all()
+num_remaining_alarms = len(list(remaining_alarms))
+print(f'{num_remaining_alarms} alarms remaining in account.')
