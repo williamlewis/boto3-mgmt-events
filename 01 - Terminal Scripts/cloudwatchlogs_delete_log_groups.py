f9eacce8fedf68cf400d8a5f17e476d3630278b0
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-13 08:38:50 -0500

    Create CloudWatchLogs delete log groups script

---
+++ 01 - Terminal Scripts/cloudwatchlogs_delete_log_groups.py
@@ -0,0 +1,5 @@
+# Find and delete log groups from CloudWatchLogs
+
+# create boto3 cloudwatchlogs client (NOT the same as cloudwatch client for alarms)
+# describe log groups to store in a list
+# delete group and print a confirmation statement
