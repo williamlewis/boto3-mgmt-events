114eb0932f9af6404209e43c317935672639dd54
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-13 08:41:16 -0500

    Finalize CloudWatchLogs delete log groups script

--- 01 - Terminal Scripts/cloudwatchlogs_delete_log_groups.py
+++ 01 - Terminal Scripts/cloudwatchlogs_delete_log_groups.py
@@ -1,5 +1,19 @@
 # Find and delete log groups from CloudWatchLogs
 
 # create boto3 cloudwatchlogs client (NOT the same as cloudwatch client for alarms)
+import boto3
+client = boto3.client('logs')
+
 # describe log groups to store in a list
+groups = client.describe_log_groups()
+group_names = []
+for group in groups['logGroups']:
+    log_group_name = group['logGroupName']
+    group_names.append(log_group_name)
+
 # delete group and print a confirmation statement
+for name in group_names:
+    client.delete_log_group(
+        logGroupName=name
+    )
+    print(f'Log Group "{name}" has been successfully deleted.')
