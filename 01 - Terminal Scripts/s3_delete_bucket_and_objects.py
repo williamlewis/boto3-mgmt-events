81ef98b8d23ce76060c4245dac7bc858c7b6e283
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-14 08:45:36 -0500

    Finalize S3 delete bucket and objects script

--- 01 - Terminal Scripts/s3_delete_bucket_and_objects.py
+++ 01 - Terminal Scripts/s3_delete_bucket_and_objects.py
@@ -1,7 +1,37 @@
 # Delete a specific S3 bucket including all objects and object versions within
 
 # create boto3 S3 resource, get bucket & objects collection
+import boto3
+s3 = boto3.resource('s3')
+
+bucket_name = 'demo-csv-upload' # update bucket name to run
+bucket = s3.Bucket(bucket_name)
+
+objects = bucket.objects.all()
+
 
 # loop over objects to delete versions, then objects, then overall bucket
+for object in objects:
+    obj_key = object.key
+    obj_bucket = object.bucket_name
+    try:
+        object.object_versions.delete()
+        print(f'Deleting object VERSIONS of object "{object.key}"')
+    except:
+        print('*Couldn\'t delete object versions*')
+    
+    try:
+        object.delete()
+        print(f'Deleting OBJECT "{object.key}" from bucket "{obj_bucket}"')
+    except:
+        print('*Couldn\'t delete object*')
+    
+    try:
+        bucket.delete()
+        print(f'Deleting BUCKET "{bucket_name}"')
+    except:
+        print('*Couldn\'t delete bucket*')
+
 
 # print statement to confirm script is complete
+print('Script complete.')
