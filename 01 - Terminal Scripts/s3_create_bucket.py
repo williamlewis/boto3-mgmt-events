7a402a75a07a82172df59ed245e2b1f6cb840711
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-15 07:03:40 -0500

    Finalize S3 create bucket script

--- 01 - Terminal Scripts/s3_create_bucket.py
+++ 01 - Terminal Scripts/s3_create_bucket.py
@@ -1,5 +1,15 @@
 # Create a new S3 bucket
 
 # set up boto3 S3 resource
+import boto3
+s3_resource = boto3.resource('s3')
+
 # provide bucket name & create bucket
+bucket_name = 'demo-csv-upload'
+
+s3_resource.create_bucket(
+    Bucket=bucket_name
+)
+
 # print confirmation to indicate end of script
+print(f'Bucket "{bucket_name}" was successfully created.')
