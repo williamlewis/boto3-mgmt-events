e3d125c92d53d85c5d0b4ab0b22fcfd16f304ff4
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-16 07:47:31 -0500

    Create deregister AMI script

---
+++ 01 - Terminal Scripts/ec2_deregister_ami.py
@@ -0,0 +1,5 @@
+# Deregister (delete) a custom machine image from a specific region
+
+# create boto3 EC2 resource for one region (AMIs live within EC2 service)
+# deregister AMI
+# print confirmation statement to indicate end of script
