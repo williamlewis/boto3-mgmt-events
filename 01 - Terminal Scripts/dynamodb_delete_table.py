c8609b5f7db5308105e853a6d65246611021d75d
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-17 08:11:27 -0500

    Finalize DynamoDB delete table script

--- 01 - Terminal Scripts/dynamodb_delete_table.py
+++ 01 - Terminal Scripts/dynamodb_delete_table.py
@@ -1,5 +1,15 @@
 # Delete a table in DynamoDB
 
-# create boto3 DynamoDB resource
-# access & delete specific table by name
+# create boto3 DynamoDB resource & get table by name
+import boto3
+dyn = boto3.resource('dynamodb')
+
+table_name = 'employees-batch-demo'
+table = dyn.Table(table_name)
+
+# delete table & get status
+response = table.delete()
+table_state = response['TableDescription']['TableStatus']
+
 # print statement to indicate end of script
+print(f'Table "{table_name}" has been successfully deleted; table status is now "{table_state}".')
