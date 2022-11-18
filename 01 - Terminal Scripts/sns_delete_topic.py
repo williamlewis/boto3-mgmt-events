e7232a04a4fed955a1871d09a9e8282689c81010
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-18 09:21:52 -0500

    Finalize SNS delete topic script

--- 01 - Terminal Scripts/sns_delete_topic.py
+++ 01 - Terminal Scripts/sns_delete_topic.py
@@ -1,4 +1,11 @@
 # Delete an SNS topic and related subscriptions
 
 # create boto3 client
+import boto3
+sns = boto3.client('sns')
+
 # delete topic and indicate end of script
+sns.delete_topic(
+    TopicArn='arn:aws:sns:us-east-1:095304811476:snapshots' # update input here
+)
+print('SNS topic has been deleted.')
