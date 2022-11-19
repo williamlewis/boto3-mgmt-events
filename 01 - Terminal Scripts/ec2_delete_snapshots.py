b427e23b01f1e30420c96dde3be4dd125445d5d7
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-19 10:54:53 -0500

    Finalize EC2 delete snapshots script

--- 01 - Terminal Scripts/ec2_delete_snapshots.py
+++ 01 - Terminal Scripts/ec2_delete_snapshots.py
@@ -1,4 +1,12 @@
 # Delete instance snapshots created by self
 
-# create boto3 EC2 resource
-# delete snapshots & print statement to indicate end of script
+# create boto3 EC2 resource & get self-owned snapshots with collection
+import boto3
+ec2 = boto3.resource('ec2')
+
+snapshots = ec2.snapshots.filter(OwnerIds=['self'])
+
+# delete snapshots & print confirmation statement
+for snapshot in snapshots:
+    snapshot.delete()
+    print(f'Snapshot with Id = {snapshot.snapshot_id} is deleted')
