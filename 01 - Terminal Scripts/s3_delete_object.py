8bb53df8d5ddcec4884c5212fdabb26768162939
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-20 13:06:52 -0500

    Finalize S3 delete object script

--- 01 - Terminal Scripts/s3_delete_object.py
+++ 01 - Terminal Scripts/s3_delete_object.py
@@ -1,5 +1,17 @@
 # Delete a single object from an S3 bucket
 
-# set up boto3 S3 client
+# set up boto3 S3 client & define bucket / object inputs
+import boto3
+client = boto3.client('s3')
+
+bucket_name = '' # update with bucket name
+key_name = '' # update with object name, including file type extension
+
 # delete named object from named bucket
+response = client.delete_object(
+    Bucket=bucket_name,
+    Key=key_name
+)
+
 # print statement to indicate end of script
+print(f'{key_name} has been deleted from {bucket_name} bucket.')
