805962e6a79910f7c98d146ce67217e63aff5f4d
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-21 16:50:43 -0500

    Finalize EC2 terminate instances script

--- 01 - Terminal Scripts/ec2_terminate_all_instances.py
+++ 01 - Terminal Scripts/ec2_terminate_all_instances.py
@@ -1,4 +1,12 @@
 # Terminate all current EC2 instances
 
-# create boto3 EC2 resource
+# create boto3 EC2 resource & get all instances with collection
+import boto3
+ec2 = boto3.resource('ec2')
+
+instances = ec2.instances.all()
+
 # terminate instances & print statement to indicate end of script
+for instance in instances:
+    instance.terminate()
+    print(f'Instance with Id = {instance.instance_id} has been successfully terminated.')
