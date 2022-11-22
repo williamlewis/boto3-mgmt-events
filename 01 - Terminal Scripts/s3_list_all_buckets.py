23d7da32120fdd394606da8beee7ab58bf80f0df
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-22 10:12:05 -0500

    Finalize S3 list all buckets script

--- 01 - Terminal Scripts/s3_list_all_buckets.py
+++ 01 - Terminal Scripts/s3_list_all_buckets.py
@@ -1,4 +1,11 @@
 # List all current S3 buckets
 
 # create boto3 S3 client
-# print bucket names from response
+import boto3
+client = boto3.client('s3')
+
+# list buckets & print names from response
+response = client.list_buckets()
+
+for bucket in response['Buckets']:
+    print(bucket['Name'])
