98a368d74766c90cd8032866e7f51dc188c9692f
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-23 10:08:49 -0500

    Finalize S3 list bucket objects script

--- 01 - Terminal Scripts/s3_list_objects_in_bucket.py
+++ 01 - Terminal Scripts/s3_list_objects_in_bucket.py
@@ -1,4 +1,18 @@
 # List all objects in a specific S3 bucket
 
 # create boto3 S3 client & define bucket
-# list objects & print object key
+import boto3
+client = boto3.client('s3')
+
+# list objects & print object key(s)
+bucket_name = '' # update bucket name here
+
+response = client.list_objects(
+    Bucket=bucket_name
+)
+
+for object in response['Contents']:
+    print(object['Key'])
+
+# indicate end of script
+print('Script complete.')
