7c9771dfdcf1eeb3dcc3d66a4c112daf3335902b
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-25 19:20:08 -0500

    Create S3 upload local file script

---
+++ 01 - Terminal Scripts/s3_upload_local_file.py
@@ -0,0 +1,6 @@
+# Upload a local file to an S3 bucket
+
+# create boto3 S3 resource
+# specify destination bucket, file path, & file name
+# use the META.Client to access "upload_file" client method through the resource class
+# print success statement to indicate end of script
