92d16b1c9800f0fc8cbd5c5a4fb45d8a474753b6
William Lewis <31929244+williamlewis@users.noreply.github.com>
2022-11-26 17:52:00 -0500

    Create S3 delete all buckets script

---
+++ 01 - Terminal Scripts/s3_delete_all_buckets_and_objects.py
@@ -0,0 +1,6 @@
+# Find and delete all S3 buckets, including objects and object versions
+
+# create boto3 S3 resource
+# print list of all found buckets (before deleting)
+# delete object versions, objects, & buckets
+# print statement to indicate end of script
